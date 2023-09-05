f48fcd64086d9ed921c80c367777d8c57d688a28
Matheus <math.av7@outlook.com>
2023-09-04 21:47:35 -0300

    feature: use SimpleBar for mobile responsiveness

--- src/components/DocumentationTabs.tsx
+++ src/components/DocumentationTabs.tsx
@@ -1,21 +1,30 @@
+"use client";
+
+import Code from "@/components/ui/Code";
+import { nodejs, python } from "@/constants/documentation-code";
 import { FC } from "react";
+import SimpleBar from "simplebar-react";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "./ui/Tabs";
-import SimpleBar from 'simplebar-react'
-import Code from "@/components/Code";
-import { nodejs } from "@/constants/documentation-code";
+import "simplebar-react/dist/simplebar.min.css";
 
 const DocumentationTabs: FC = ({}) => {
   return (
     <Tabs defaultValue="nodejs" className="max-w-2xl w-full">
       <TabsList>
-        <TabsTrigger value='nodejs'>NodeJS</TabsTrigger>
-        <TabsTrigger value='python'>Python</TabsTrigger>
+        <TabsTrigger value="nodejs">NodeJS</TabsTrigger>
+        <TabsTrigger value="python">Python</TabsTrigger>
       </TabsList>
 
-      <TabsContent value='nodejs'>
-        <Code language="javascript" code={nodejs} show animated/>
+      <TabsContent value="nodejs">
+        <SimpleBar>
+          <Code language="javascript" code={nodejs} show animated />
+        </SimpleBar>
       </TabsContent>
-      <TabsContent value="python"></TabsContent>
+      <TabsContent value="python">
+        <SimpleBar>
+          <Code language="python" code={python} show animated />
+        </SimpleBar>
+      </TabsContent>
     </Tabs>
   );
 };
